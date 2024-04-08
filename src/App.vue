e76fb1d6857eb5be0417ffb298c6c5a9ab8ba274
erickhenrique3 <erick.2henriquehtp@gmail.com>
2024-04-08 10:32:57 -0300

    apredendo vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -6,10 +7,12 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    
   }
 }
 </script>
